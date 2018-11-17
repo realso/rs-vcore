42c60ff93ecbd83057a11f8d0cb505bb1317257b
ggtiger <wh123456>
2018-11-17 17:45:32 +0800

    入口文件修改

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { DataTable } from "./store/DataTable";
-import BaseStore from "./store/base";
-import BaseService from "./service/base";
-export { DataTable, BaseStore, BaseService }
+import Store01 from "./store/Store01";
+import Service01 from "./service/Service01";
+export { DataTable, Store01, Service01 }
